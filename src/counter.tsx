918505d9be0e11a738af8d99af608cbad3289e01
Coffey May <jcoffeymay6@gmail.com>
2021-08-26 21:27:55 +0000

    m

---
+++ src/counter.tsx
@@ -0,0 +1,22 @@
+import { useState }  from "react";
+
+export const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    const addNumber = () => {
+      return setCount(count + 1)
+    }
+
+    const subtractNumber = () => {
+      return setCount(count - 1)
+    }
+
+    return(
+    <>
+        <h2>Counter</h2>
+        <h4>{count}</h4>
+        <button title="addition" onClick={addNumber} id="add">Add</button>
+        <button title="subtraction" onClick={subtractNumber} id="subtract">Subtract</button>
+    </>
+    )
+}
---
+++ src/greetingList.tsx
@@ -0,0 +1,41 @@
+import { useState }  from "react";
+
+let nameData = [{name:"joe"},{name:"jane"},{name:"jerry"}]
+
+
+export const GreetingList = () => {
+    const [greeted, setGreeted] = useState(nameData)
+    const [inputText,setInputText] = useState("")
+
+    class Person {
+        name: any;
+        constructor(name: string) {
+        this.name = name;
+    }
+    }
+  
+
+    const handleAddName = (e) => {
+        e.preventDefault()
+        nameData.push(new Person(inputText))
+        setGreeted(nameData)
+        setInputText("")
+    }
+
+    const handleInputChange = (e) => {
+        setInputText(e.target.value)
+    }
+
+    return(
+    <>
+        <h2>Greeting List</h2>
+        <form onSubmit={handleAddName} action="">
+        <label htmlFor="">Enter a name </label>
+        <input value={inputText} placeholder="enter name here" onChange={handleInputChange} type="text"/>
+        </form>
+        {greeted.map((p)=>(
+        <p>Welcome to unit testing {p.name}.</p>
+        ))}
+    </>
+    )
+}
