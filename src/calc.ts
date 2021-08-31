edc81526316970af96815dbaeaafce25955d6bb1
Coffey May <jcoffeymay6@gmail.com>
2021-08-31 01:47:39 +0000

    testing study

--- src/calc.ts
+++ src/calc.ts
@@ -1,24 +1,20 @@
 
 export const sum = (numbers) => {
     let nums = numbers.split(",")
- 
-console.log(nums)
     if (nums == ""){return 0}
+    if (nums.length === 1) {return parseInt(nums[0])}
+    if (nums.length === 2) {return parseInt(nums[0]) + parseInt(nums[1])}
+    if (nums.length >= 3) {
+           var numberArray = []
+   for (var i = 0; i < nums.length; i++)
+        numberArray.push(parseInt(nums[i]));
+              let numArr = numberArray.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue
+        },0)
+        return numArr
+}
+}
     
 
-    if (nums.length === 1) {
-        return parseInt(nums[0])
-    } else if (nums.length === 2) {
-        return parseInt(nums[0]) + parseInt(nums[1])
-    }else if (nums.length > 2){
-      for (let i =0;i<nums.length;i++){
-          let output =0
-          console.log(nums[i])
-         return  output += parseInt(nums[i])
-          
 
-      }
-    }
-
-}
 
